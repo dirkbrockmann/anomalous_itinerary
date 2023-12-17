2738daa50b9199a904008eeb82e44f1914e9a486
Dirk Brockmannn <brockmann.dirk@gmail.com>
2023-12-17 11:07:21 +0100

    added halt function, updated border setup

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import setup_container from "./setup_container.js"
 import setup_interactions from "./setup_interactions.js"
 import setup_controls from "./controls.js"
 import {initialize as setup_simulation} from "./simulation.js"
+import {go} from "./controls.js"
 
 // load is called in the webpage
 // the container configuration can be passed as an argument, including classes and styles for the 
@@ -14,15 +15,17 @@ import {initialize as setup_simulation} from "./simulation.js"
 // the function below usually doesn't have to change structurally. It is advisable to comment
 // out steps from below to work on the code that is called sequentially
 
+var display,controls,grid;
+
 const load = function (container_id,config=cfg) {
 	
 // setting up the container
 	
 	const container = setup_container(container_id,config);
 
-	const display = container.display;
-	const controls = container.controls;
-	const grid = container.grid;
+	display = container.display;
+	controls = container.controls;
+	grid = container.grid;
 		
 // setting up the controls and actions
 	
@@ -36,4 +39,10 @@ const load = function (container_id,config=cfg) {
 	
 }
 
-export {load,cfg as config};
+const halt  = function(){
+	if(go.value()==1){
+			go.press(controls)
+	}
+}
+
+export {load,cfg as config,halt};
