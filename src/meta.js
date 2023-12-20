097b98c7e1d9aabd20fbf01dc5df7d2e6ec3da9a
Dirk Brockmannn <brockmann.dirk@gmail.com>
2023-12-20 08:22:54 +0100

    added meta.js

---
+++ src/meta.js
@@ -0,0 +1,5 @@
+export default {
+	title: "Anomalous Itinerary",
+	subtitle: "A visual illustration of Lévy Flights",
+	description: "The explorable illustrates the properties of superdiffusive, scale-free random walks known as Lévy flights."
+}
